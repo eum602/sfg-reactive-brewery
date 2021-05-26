18a7d57bf3c972a8c0a7f309f04b8252f8e3267c
Erick <eum602@gmail.com>
2021-05-26 09:46:19 -0500

    implements INTEGRATION TESTS for 'saveBeerBadRequest' - modifies Handler as needed -  adds Validator to handle the exception

--- src/main/java/guru/springframework/sfgrestbrewery/web/functional/BeerHandlerV2.java
+++ src/main/java/guru/springframework/sfgrestbrewery/web/functional/BeerHandlerV2.java
@@ -5,18 +5,27 @@ import guru.springframework.sfgrestbrewery.web.model.BeerDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.ServerWebInputException;
 import reactor.core.publisher.Mono;
+
+
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class BeerHandlerV2 {
     private final BeerService beerService;
+    private final Validator validator; //standard spring validator
 
     public Mono<ServerResponse> saveNewBeer(ServerRequest request){
-        Mono<BeerDto> beerDtoMono =  request.bodyToMono( BeerDto.class);
+        Mono<BeerDto> beerDtoMono =  request
+                .bodyToMono( BeerDto.class)
+                .doOnNext(this::validate);
         return beerService.saveNewBeerMono(beerDtoMono)
                 .flatMap(beerDto -> {
                     return ServerResponse
@@ -24,6 +33,17 @@ public class BeerHandlerV2 {
                             .header("location",BeerRouterConfig.BEER_V2_URL +  "/" +beerDto.getId())
                             .build();
                 });
+
+    }
+
+    private void validate(BeerDto beerDto){
+        Errors errors = new BeanPropertyBindingResult(beerDto, "beerDto");
+        validator.validate(beerDto,errors);
+
+        if (errors.hasErrors()){
+            throw new ServerWebInputException(errors.toString());//Withing the webflux framework when a webserver produces an input exception
+            //that will cause a bad request exception
+        }
 
     }
 
